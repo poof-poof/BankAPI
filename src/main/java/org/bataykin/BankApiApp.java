60d0db8d9b53c7ec115d7bfd1edb28c88c1555b4
Alexey <lexa10.95@mail.ru>
2020-11-27 12:59:33 +0300

    [BA-0] Create base structure for api;

---
+++ src/main/java/org/bataykin/BankApiApp.java
@@ -0,0 +1,11 @@
+package org.bataykin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiApp.class, args);
+    }
+}
